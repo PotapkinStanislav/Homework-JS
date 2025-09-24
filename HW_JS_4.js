f73e05c6160a99d706d43c5e08374cb2073372e6
Potapkin Stanislav <psi130586@gmail.com>
2025-09-24 18:20:45 +0300

    New_homework

---
+++ HW_JS_4.js
@@ -0,0 +1,43 @@
+var services = {
+  "стрижка": "60 грн",
+  "гоління": "80 грн",
+  "Миття голови": "100 грн"
+};
+
+
+services.price = function () {
+  let sum = 0;
+  for (let key in this) {
+    if (typeof this[key] === "string" && this[key].includes("грн")) {
+      sum += parseInt(this[key]);
+    }
+  }
+  return sum + " грн";
+};
+
+services.minPrice = function () {
+  let prices = [];
+  for (let key in this) {
+    if (typeof this[key] === "string" && this[key].includes("грн")) {
+      prices.push(parseInt(this[key]));
+    }
+  }
+  return Math.min(...prices) + " грн";
+};
+
+services.maxPrice = function () {
+  let prices = [];
+  for (let key in this) {
+    if (typeof this[key] === "string" && this[key].includes("грн")) {
+      prices.push(parseInt(this[key]));
+    }
+  }
+  return Math.max(...prices) + " грн";
+};
+
+services['Манікюр'] = "200 грн";
+
+
+console.log("Загальна вартість наданих послуг:", services.price());
+console.log("Мінімальна ціна:", services.minPrice());
+console.log("Максимальна ціна:", services.maxPrice());
